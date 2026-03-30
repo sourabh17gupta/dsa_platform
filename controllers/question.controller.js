307570dd8b634d5865a0ee985a70f4723d814ece
Sourabh Gupta <123104060@nitkkr.ac.in>
2026-03-30 19:34:21 +0530

    from backend-question question route and controller created

--- controllers/question.controller.js
+++ controllers/question.controller.js
@@ -31,7 +31,7 @@ exports.questionCreatecontroller = async(req,res)=>{
             questionId:qId,
             input:testcase.input,
             output:testcase.output,
-            image:testcase.image,
+            image:testcase?.image,
             explanation:testcase.explanation,
            })
        }
@@ -40,6 +40,57 @@ exports.questionCreatecontroller = async(req,res)=>{
         message:"question saved successfully"
        })
 
+    }
+    catch(err){
+        res.status(500).json({
+           message:"error while creating new question in database "+err,
+        })
+    }
+}
+
+/**
+ * @name singleQuestionController
+ * @description route to get data of question in database
+ * @access public
+ */
+
+exports.singleQuestionController = async(req,res)=>{
+    try{
+       const {questionId} = req.params;
+
+       const data = await QuestionModel.findById(questionId);
+       const testcases = await testcaseModel.find({
+        questionId : questionId
+       });
+
+       res.status(200).json({
+        message : "question data fetch successfully",
+        data,
+        testcases
+       })
+    }
+    catch(err){
+        res.status(500).json({
+           message:"error while creating new question in database "+err,
+        })
+    }
+}
+
+
+/**
+ * @name getAllQuestionController
+ * @description route to question id and its its heading
+ * @access public
+ */
+
+exports.getAllQuestionController = async(req,res)=>{
+    try{
+       const data = await QuestionModel.find().select("_id heading type");
+
+       res.status(200).json({
+        message : "get all question",
+        data
+       })
     }
     catch(err){
         res.status(500).json({
